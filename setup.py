65f07fcef86d7406bcccdf681b19871e9372fdb2
Peter Cottle <pcottle@fb.com>
2015-05-02 15:51:11 -0700

    moved folder

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 
 setup(
     name='pcottle-pip-example',
-    version='0.0.2',
+    packages=['pcottle-pip-example'],
+    version='0.0.3',
     author=u'Peter Cottle',
     author_email='petermcottle@gmail.com',
     url='https://github.com/pcottle/example_for_pip',
