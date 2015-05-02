4326ed7d0f452fd80366df988f8a6ff85fb646c1
Peter Cottle <pcottle@fb.com>
2015-05-02 15:26:47 -0700

    with dist

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from distutils.core import setup
 
 setup(
     name='pcottle-pip-example',
-    version='0.0.1',
+    version='0.0.2',
     author=u'Peter Cottle',
     author_email='petermcottle@gmail.com',
     url='https://github.com/pcottle/example_for_pip',
     license='BSD licence, see LICENCE.txt',
     description='print out a simple statement',
-    zip_safe=True,
 )
