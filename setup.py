7569ffce6fcf531f8835743ab08c7e1ac8542c3e
Peter Cottle <pcottle@fb.com>
2015-05-02 15:04:16 -0700

    everything i guesssss

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version='0.0.1',
     author=u'Peter Cottle',
     author_email='petermcottle@gmail.com',
-    packages=['pcottlepip'],
     url='https://github.com/pcottle/example_for_pip',
     license='BSD licence, see LICENCE.txt',
     description='print out a simple statement',
