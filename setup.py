054b049a147f89d96775798db72bd42375910046
Peter Cottle <pcottle@fb.com>
2015-05-02 14:59:54 -0700

    git push

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='pcottle-pip-example',
+    version='0.0.1',
+    author=u'Peter Cottle',
+    author_email='petermcottle@gmail.com',
+    packages=['pcottlepip'],
+    url='https://github.com/pcottle/example_for_pip',
+    license='BSD licence, see LICENCE.txt',
+    description='print out a simple statement',
+    zip_safe=True,
+)
