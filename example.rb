96160afc93228dd4245a2fbac82eaf6dd50e62fb
Peter Cottle <pcottle@fb.com>
2015-05-03 12:10:13 -0700

    more progress

--- example.rb
+++ example.rb
@@ -1,14 +1,14 @@
 class Example < Formula
   url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.tar.gz"
-  sha256 "2f51b3d604ded561b376617857876ff4fa03ce85484abe9b9f068f6bad25994d"
-  version "0.0.2"
+  sha256 "038753f944edf7b3d6b0dbcf0056350eead1e14db4a3c70fafb711e9a31be3d4"
+  version "0.0.3"
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
 
   def install
     puts 'Unpacking PathPicker'
-    system "tar xopf /Library/Caches/Homebrew/example-0.0.2.tar.gz -C /Users/pcottle/Desktop/"
+    system "tar xopf /Library/Caches/Homebrew/example-0.0.3.tar.gz -C /Users/pcottle/Desktop/"
     puts 'Symlinking bash script'
-    system "ln -s /Users/pcottle/Desktop/example_for_pip/pcottlepipexample/example /usr/local/bin/pcottleexample"
+    system "ln -s /Users/pcottle/Desktop/example /usr/local/bin/pcottleexample"
   end
 
 end
