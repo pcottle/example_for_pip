22a4145134778182babbe8328fef674a591b4ec4
Peter Cottle <pcottle@fb.com>
2015-05-03 12:18:27 -0700

    going to RFC

--- pcottleexample.rb
+++ pcottleexample.rb
@@ -1,4 +1,6 @@
 class Pcottleexample < Formula
+  homepage "https://facebook.github.io/PathPicker/"
+  head "https://github.com/facebook/pathpicker/"
   url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.tar.gz"
   sha256 "038753f944edf7b3d6b0dbcf0056350eead1e14db4a3c70fafb711e9a31be3d4"
   version "0.0.3"
@@ -8,7 +10,7 @@ class Pcottleexample < Formula
     puts 'Unpacking PathPicker'
     system "tar xopf /Library/Caches/Homebrew/example-0.0.3.tar.gz -C /Users/pcottle/Desktop/"
     puts 'Symlinking bash script'
-    system "ln -f -s /Users/pcottle/Desktop/example /usr/local/bin/pcottleexample"
+    ln_sf '/Users/pcottle/Desktop/example', '/usr/local/bin/pcottleexample'
   end
 
 end
