3acdce5706e0df5b05c681204268442e25f14c2f
Peter Cottle <pcottle@fb.com>
2015-05-03 13:32:04 -0700

    new version

--- fpp.rb
+++ fpp.rb
@@ -1,7 +1,7 @@
 class Fpp < Formula
   homepage "https://facebook.github.io/PathPicker/"
   # To be changed -- just a sample tar for now
-  url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.0.0.3.tar.gz"
+  url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.0.0.4.tar.gz"
   sha256 "038753f944edf7b3d6b0dbcf0056350eead1e14db4a3c70fafb711e9a31be3d4"
   head "https://github.com/facebook/pathpicker.git"
 
