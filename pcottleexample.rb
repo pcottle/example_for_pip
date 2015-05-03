c8ec958c8d7b2f28ec8e2fedf54d086ae3fb54c9
Peter Cottle <pcottle@fb.com>
2015-05-03 12:11:35 -0700

    final

--- pcottleexample.rb
+++ pcottleexample.rb
@@ -1,4 +1,4 @@
-class Example < Formula
+class Pcottleexample < Formula
   url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.tar.gz"
   sha256 "038753f944edf7b3d6b0dbcf0056350eead1e14db4a3c70fafb711e9a31be3d4"
   version "0.0.3"
@@ -8,7 +8,7 @@ class Example < Formula
     puts 'Unpacking PathPicker'
     system "tar xopf /Library/Caches/Homebrew/example-0.0.3.tar.gz -C /Users/pcottle/Desktop/"
     puts 'Symlinking bash script'
-    system "ln -s /Users/pcottle/Desktop/example /usr/local/bin/pcottleexample"
+    system "ln -f -s /Users/pcottle/Desktop/example /usr/local/bin/pcottleexample"
   end
 
 end
