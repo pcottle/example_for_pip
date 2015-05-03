b0bcfdb3dc66355743a4f0e59b54f2bce9948dfc
Peter Cottle <pcottle@fb.com>
2015-05-03 12:02:03 -0700

    progress

--- example.rb
+++ example.rb
@@ -1,12 +1,14 @@
 class Example < Formula
-  url "https://pypi.python.org/packages/source/p/pcottlepipexample/pcottlepipexample-0.0.5.tar.gz"
-  sha256 "96ddbcd7ee6142c5f478ef5a1c51d81eb5d96ef849673632a252ccea121635d9"
+  url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.tar.gz"
+  sha256 "2f51b3d604ded561b376617857876ff4fa03ce85484abe9b9f068f6bad25994d"
+  version "0.0.2"
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
 
   def install
     puts 'Unpacking PathPicker'
-    system "tar xopf /Library/Caches/Homebrew/example-0.0.5.tar.gz -C /Users/pcottle/Desktop/"
+    system "tar xopf /Library/Caches/Homebrew/example-0.0.2.tar.gz -C /Users/pcottle/Desktop/"
     puts 'Symlinking bash script'
     system "ln -s /Users/pcottle/Desktop/example_for_pip/pcottlepipexample/example /usr/local/bin/pcottleexample"
   end
+
 end
