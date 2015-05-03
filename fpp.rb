4994d936d0138b668efa98352eaf2a7e73f2570f
Peter Cottle <pcottle@fb.com>
2015-05-03 13:33:07 -0700

    YES WORKS

--- fpp.rb
+++ fpp.rb
@@ -2,7 +2,7 @@ class Fpp < Formula
   homepage "https://facebook.github.io/PathPicker/"
   # To be changed -- just a sample tar for now
   url "https://github.com/pcottle/example_for_pip/raw/master/dist/pcottleexample.0.0.4.tar.gz"
-  sha256 "038753f944edf7b3d6b0dbcf0056350eead1e14db4a3c70fafb711e9a31be3d4"
+  sha256 "25f3edddc28b7a353c407d48a74a92f9a364160de790122357a3785d9d9e8aeb"
   head "https://github.com/facebook/pathpicker.git"
 
   depends_on :python if MacOS.version <= :snow_leopard
